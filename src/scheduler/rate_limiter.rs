5b79b134f77182c2a1b504965d5295ee143accf1
xetera <mythologyali@gmail.com>
2021-08-03 14:14:52 +0300

    rate limiter documentation cleanup

--- src/scheduler/rate_limiter.rs
+++ src/scheduler/rate_limiter.rs
@@ -6,14 +6,15 @@ use governor::{
 use nonzero_ext::nonzero;
 use std::time::Duration;
 
-/// Most providers use rate limited at the domain level and not at the page level
+/// Most providers use rate limiter at the domain level and not at the page level
 /// in order to prevent exceeding rate limits imposed by webservers
-/// The same rate limiter is also used by
 pub type UnscopedLimiter = RateLimiter<NotKeyed, InMemoryState, QuantaClock>;
+
 /// Some providers can use rate limiting at the page level imposed by set limits of API keys
 #[allow(dead_code)]
 pub type ScopedLimiter = RateLimiter<dyn DirectStateStore, InMemoryState, QuantaClock>;
 
+/// Global rate limiting wrapper for limits imposed on individual providers being run concurrently
 pub struct GlobalRateLimiter(UnscopedLimiter);
 
 const PROVIDER_PROCESSING_LIMIT: u32 = 8;
@@ -27,9 +28,12 @@ pub fn global_rate_limiter() -> GlobalRateLimiter {
     GlobalRateLimiter(rate_limiter)
 }
 
+/// Waits for a global rate limit to be up
 pub async fn wait_provider_turn(limiter: &GlobalRateLimiter) {
     limiter
         .0
-        .until_ready_with_jitter(Jitter::up_to(Duration::from_secs(4u64)))
+        // jitter to prevent multiple queued providers from trying to run all at once
+        // after the rate limiter is available again
+        .until_ready_with_jitter(Jitter::up_to(Duration::from_secs(2u64)))
         .await;
 }
