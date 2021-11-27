65d6a62d9d5fc27735f9c5314a08f7821ac0ae6c
xetera <mythologyali@gmail.com>
2021-11-27 16:59:42 +0300

    removed frontend

--- site/src/theme.tsx
+++
@@ -1,8 +0,0 @@
-import { ChakraTheme, extendTheme } from "@chakra-ui/react";
-
-export const theme: Partial<ChakraTheme> = extendTheme({
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: false
-  }
-})
